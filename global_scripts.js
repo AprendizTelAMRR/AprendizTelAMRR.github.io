b201f6ea844c8594b856cf4037aa8039fae38aef
AprendizTelAMRR <aprendiz.operacionaltel@outlook.com>
2023-10-13 11:50:23 -0400

    change style test

--- global_scripts.js
+++ global_scripts.js
@@ -1,17 +1,23 @@
 var bgColRed = document.getElementsByClassName("bgcol-red")
 var bgColBlue = document.getElementsByClassName("bgcol-blue")
 var fgColRed = document.getElementsByClassName("fgcol-red")
+var fgColBlue = document.getElementsByClassName("fgcol-blue")
 
 for (var i = 0; i < bgColRed.length; ++i) {
     bgColRed[i].style.backgroundColor = "#C92230";
 }
 
 for (var i = 0; i < bgColBlue.length; ++i) {
-    bgColBlue[i].style.backgroundColor = "#032030";
+    bgColBlue[i].style.backgroundColor = "#202A6B";
 }
 
 for (var i = 0; i < fgColRed.length; ++i) {
     fgColRed[i].style.backgroundColor = "#D9A7AB";
+}
+
+for (var i = 0; i < fgColBlue.length; ++i) {
+    fgColBlue[i].style.backgroundColor = "#92C2D1";
+    //20596B
 }
 
 //HAMBURGER MENU
